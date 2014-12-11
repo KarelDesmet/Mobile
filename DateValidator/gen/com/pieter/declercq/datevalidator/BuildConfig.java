2198bc776e09a940bd2079d5befd48e0ed2eedea
Pieter <Pieter@Laptop_Pieter>
2014-12-11 20:55:04 +0100

    CategoryPicker UI afgewerkt

--- DateValidator/gen/com/pieter/declercq/datevalidator/BuildConfig.java
+++ DateValidator/gen/com/pieter/declercq/datevalidator/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.datevalidator;
+package com.pieter.declercq.datevalidator;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
