23ef53c73fe939682e7678ae226a87c97ca97639
Pieter <Pieter@Laptop_Pieter>
2014-12-11 03:02:51 +0100

    Functionaliteit product toevoegen (werkend) incl. JUnit & commentaar

--- ProductValidator/src/test/tester.java
+++ ProductValidator/src/test/tester.java
@@ -1,11 +1,14 @@
 package test;
 
 import service.DateValidator;
+import domain.Product;
 
 public class tester {
 
 	public static void main(String[] args) {
 		DateValidator d = new DateValidator();
+		d.addProduct("charcuterie", new Product(9781780161389L,
+				"Linear Algebra", "Pearson"));
 		System.out.println(d.getEanDatabase());
 	}
 
