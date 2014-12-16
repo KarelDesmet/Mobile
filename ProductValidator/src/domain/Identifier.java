7cf4201b9cb12e7ea19be49090b4ef11d9215f82
Pieter <Pieter@Laptop_Pieter>
2014-12-16 14:42:05 +0100

    Nieuwe dimensies + aanpassing identifier

--- ProductValidator/src/domain/Identifier.java
+++ ProductValidator/src/domain/Identifier.java
@@ -25,16 +25,19 @@ public class Identifier {
 	}
 
 	/**
-	 * A method which sets the EAN to a given Long of 13 ciphers.
+	 * A method which sets the EAN to a given Long of 8 or 13 ciphers.
 	 * 
 	 * @param ean
 	 *            The new EAN
 	 * @throws DomainException
-	 *             If the ean doesn't consist of 13 ciphers
+	 *             If the ean doesn't consist of 8 or 13 ciphers
 	 */
 	public void setEan(Long ean) throws DomainException {
-		if (ean < 1000000000000L && ean > 9999999999999L) {
-			throw new DomainException("The EAN doesn't have 13 ciphers");
+		if (ean < 1000000000000L || ean > 9999999999999L) { // EAN13
+			if (ean < 10000000L || ean > 99999999L) { //EAN8
+				throw new DomainException(
+						"The EAN doesn't have exact 8 or 13 ciphers");
+			}
 		}
 		this.ean = ean;
 	}
